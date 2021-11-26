27d2547934458850e9f2e3a803bce43bf4a1ff44
github-action-benchmark <github@users.noreply.github.com>
2021-11-26 10:11:03 +0000

    add Leger Bench for PostgreSQL (go) benchmark result for e678625f6356a82b7653a00232faa87057f316d9

---
+++ dev/bench-pgsql/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1637921463121,
+  "repoUrl": "https://github.com/numary/ledger",
+  "entries": {
+    "Leger Bench for PostgreSQL": [
+      {
+        "commit": {
+          "author": {
+            "name": "numary",
+            "username": "numary"
+          },
+          "committer": {
+            "name": "numary",
+            "username": "numary"
+          },
+          "id": "e678625f6356a82b7653a00232faa87057f316d9",
+          "message": "feature(ci): Add bench output in PR",
+          "timestamp": "2021-11-25T14:38:49Z",
+          "url": "https://github.com/numary/ledger/pull/78/commits/e678625f6356a82b7653a00232faa87057f316d9"
+        },
+        "date": 1637921462319,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkTransaction_20_1k",
+            "value": 10174147800,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
