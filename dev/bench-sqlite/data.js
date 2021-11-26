bbf19db2c00623638806bbb94f0c6a63fb353c16
github-action-benchmark <github@users.noreply.github.com>
2021-11-26 10:37:25 +0000

    add Leger Bench for SQLite (go) benchmark result for 7322e3974f6a22abbfa3a5d27bfd9da4bcf5e563

--- dev/bench-sqlite/data.js
+++ dev/bench-sqlite/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637921437812,
+  "lastUpdate": 1637923045333,
   "repoUrl": "https://github.com/numary/ledger",
   "entries": {
     "Leger Bench for SQLite": [
@@ -26,6 +26,32 @@ window.BENCHMARK_DATA = {
             "value": 1020080723,
             "unit": "ns/op",
             "extra": "1 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "numary",
+            "username": "numary"
+          },
+          "committer": {
+            "name": "numary",
+            "username": "numary"
+          },
+          "id": "7322e3974f6a22abbfa3a5d27bfd9da4bcf5e563",
+          "message": "feature(ci): Add bench output in PR",
+          "timestamp": "2021-11-25T14:38:49Z",
+          "url": "https://github.com/numary/ledger/pull/78/commits/7322e3974f6a22abbfa3a5d27bfd9da4bcf5e563"
+        },
+        "date": 1637923043970,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGetAccount",
+            "value": 18640683,
+            "unit": "ns/op",
+            "extra": "55 times\n2 procs"
           }
         ]
       }
